04abb50a6de1e9b9bfd5ffd829187f2876f1cf8a
Dev Muliya <87938020+Devmuliya16@users.noreply.github.com>
2022-08-08 13:18:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//api zqldtSxZp7XqCNBViiqvCVGsLWio0WLO
+const api ="zqldtSxZp7XqCNBViiqvCVGsLWio0WLO";
+const url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${api}`;
+
+let content;
+fetch(url).then(responce => responce.json())
+.then((data)=>{
+    console.log(data);
+
+    //send the data to the show function
+    showdata(JSON.stringify(data));
+});
+
+
+//show function
+function showdata(data){
+    console.log(data);
+    document.getElementsByClassName('content')[0].innerHTML=data;
+}
+
