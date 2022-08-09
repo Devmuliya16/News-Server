724dad9137bcfba7b76bce198551aa682f4e2d78
Dev Muliya <87938020+Devmuliya16@users.noreply.github.com>
2022-08-09 17:56:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,169 +1,219 @@
-//api zqldtSxZp7XqCNBViiqvCVGsLWio0WLO
-const api ="zqldtSxZp7XqCNBViiqvCVGsLWio0WLO";
-const url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${api}`;
+body{
+    margin: 0px;
+    padding: 0px;
+    top: 0;
+    font-family: sans-serif;
+    min-width: 250px;
+}
+/* button hover effect */
+button:hover{
+    background-color: transparent;
+    color: white;
+    box-shadow: inset 0 0 5px 0px white;
+    transition: 0.2s;
+}
+button:active{
+    background-color: gray;
+    text-shadow: 0 0 5px white;
+    transition: 0.2s;
+}
+.navbar{
+    display: flex;
+    position: sticky;
+    top: -65px;
+    z-index: 5;
+    box-shadow: 0 -1px 20px 0 black ;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-items: center;
+    background-color: black;
+    color: white;
+    padding: 1px;
+}
+.right{
+    margin-left: auto;
+}
+.right button,.right input{
+    height: 40px;
+    border-radius: 20px;
+    font-size: 15px;
+    outline: none;
+    padding: 10px;
+}
+.dropcontainer{
+    display: inline-block;
+    border-radius: 20px;
+    position: relative;
+    cursor: pointer;
+}
+.droplist{
+    position: absolute;
+    z-index: 2;
+    color: black;
+    background-color: white;
+    box-shadow: 0 0 5px 0 black;
+    display: none;
+    top: 25px;
+    padding-left: 0px;
+    padding: 0px;
+}
+.droplist li{
+    list-style: none;
+    margin: 5px;
+    padding: 5px;
+    font-size: 20px;
+    display: flex;
+    transition: 0.3s;
+}
+.droplist li img{
+    width: 25px;
+    height: 25px;
+    margin-right: 5px;
+    display: block;
+}
+.droplist li:hover{
+    background-color: black;
+    color: white;
+}
+.droplist li:active{
+    color: rgb(128, 128, 172);
+}
+
+/* hover effect */
+.dropcontainer:hover .droplist{
+    display: block;
+}
+
+
+#header{
+    position: absolute;
+    left: 0px;
+    padding: 5px;
+    z-index: 2;
+}
+#header span{
+    color: white;
+    box-shadow: 2px 2px 4px 0px black;
+    background-color: rgb(128, 128, 172);
+    padding: 5px;
+    margin: 5px;
+    border-radius: 10px;
+}
+.container{
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+}
 
 
 
-// fontawsome
-const tick = '<i class="fa-solid fa-check"></i>';
-const trend = '<i class="fa-solid fa-arrow-trend-up"></i>';
-const err = '<i class="fa-solid fa-triangle-exclamation"></i>';
-const wrong = '<i class="fa-solid fa-xmark"></i>';
-const search = '<i class="fa-solid fa-magnifying-glass"></i>';
-
-//for creating the element and uppending it
-//common for all
-function createUppend(topic,title,abstract,news,img,time){
-    let element = document.createElement('div');
-    element.className='element';
-    element.innerHTML=`
-    <img src='${img}' alt='image'>
-    <div class='text'>
-    <div class="topic">Topic: ${topic}</div>
-            <h3 class="headline">
-            ${title}
-            </h3>        
-            <h4>${news}</h4>
-            <div class="topic">${abstract}</div>
-            <br>
-            <span class="time"><b>time:</b> ${time}</span>
-            </div>
-            `;
-            
-            document.getElementsByClassName('container')[0].appendChild(element);
-        }
-//for about tags common for all
-function setheader(topic,count,fontawsome){
-    let tagcontainer = document.createElement('span');
-    tagcontainer.id='header';
-    tagcontainer.style.display='block';
-    tagcontainer.innerHTML=`<span> ${topic} ${fontawsome[0]} </span>
-                            <span> showing results: ${count} ${fontawsome[1]}</span>`;
-    document.getElementsByClassName('container')[0].appendChild(tagcontainer);
+/* news elements */
+.element{
+    background-color: rgb(224, 222, 255);
+    box-shadow: 0px 0px 6px 0px black;
+    display: block;
+    width: 620px;
+    border-radius: 5px;
+    overflow: hidden;
+    margin: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+.element img{
+    width:100%;
+}
+.text{
+    margin: 20px;
+}
+.footer{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+}
+.footer i{
+    font-size: 30px;
+    color: rgb(128, 128, 172);
+}
+.footer i:hover{
+    color: black;
+}
+.footer i:active{
+    color: rgb(128, 128, 172);
 }
 
 
 
 
+/* for copyright section */
+.copyright{
+    background-color: rgba(0, 0, 0, 0.959);
+    color: white;
+    font-size: 20px;
+    padding-left: 10px;
+    position: relative;
+    bottom: 0;
+    width: auto;
+    padding: 20px;
+}
 
-
-//fetch for home
-fetch(url)
-.then((responce) => responce.json())
-.then((data)=>{
-    //send the data to the show function
-    showdata(data);
-});
-//looping and appending for home
-function showdata(data){
-    let newsArr = data.results;
-    if(newsArr.length!=0){  
-
-        let arr = [trend,tick];
-        setheader('News in trend',newsArr.length,arr);
-
-        let topic,head,abstract,image,date;
-        for(obj of newsArr){
-            topic = (obj.section =='') ? 'topic' : obj.section;
-            head = (obj.title =='') ? 'title' : obj.title;
-            abstract = (obj.abstract =='') ? 'abstract' : obj.abstract;
-            image = (obj.multimedia == null) ? 'image' : obj.multimedia[1].url;
-            date = (obj.published_date == '') ? '2022' : obj.published_date;
-            if(obj.multimedia!=null)
-                createUppend(topic,head,abstract,``,`${image}`,date);
-        }
-    }
-    else{
-        let arr=[err,wrong];
-        setheader('Error: try again after some time ',newsArr.length,arr);
-    }
+.upbtndiv{
+    position:fixed;
+    background-color: black;
+    padding: 5px;
+    bottom: 20px;
+    right: 20px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: none;
+    z-index: 5;
+    transition:2s;
+}
+#upbtn{
+    width: inherit;
+    height: inherit;
+    border-radius: inherit;
+    font-size: larger;
 }
 
 
-
-
-//fetch for countries
-function country(countryName){
-    let url2= `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${countryName}&api-key=${api}`;
-    fetch(url2)
-    .then(responce => responce.json())
-    .then((data)=>{
-        //send the data to the show function
-        showdataSearch(data,countryName);
-    });
+.progress-bar{
+    position: sticky;
+    z-index: 10;
+    bottom: 0px;
+    width: 100%;
+    height: 5px;
+    display: block;
+    background-color: rgb(128, 128, 172);
+    box-shadow:0px 0px 7px 1px rgb(128, 128, 172) ;
+    border-radius: 20px;
 }
 
-
-
-//fetch for searching
-let findbtn = document.getElementById('searchbtn');
-findbtn.addEventListener('click',()=>{
-    let searchContent = document.getElementById('searchinput').value;
-    let url3 = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchContent}&api-key=${api}`;
-
-    fetch(url3)
-    .then(responce=> responce.json())
-    .then((data)=>{
-        showdataSearch(data,searchContent);
-    });
-})
-
-//looping and uppending for search and country
-function showdataSearch(data,name){
-    let searchNewsarr=data.response.docs;
-    document.getElementsByClassName('container')[0].innerHTML='';
-
-    if(searchNewsarr.length!=0){
-
-        let arr=[search,tick];
-        setheader(`results for: ${name}`,searchNewsarr.length,arr);
-
-        let topic,head,content,image,time,abstract;
-        for([index,element] of searchNewsarr.entries()){
-            topic = (element.keywords.length==0) ? 'topic' :  element.keywords[0].value;
-            head =  (element.headline.main==null) ? 'head' : element.headline.main;
-            abstract = (element.abstract == '') ? 'abstract' : element.abstract; 
-            content = (element.lead_paragraph=='') ? 'content' : element.lead_paragraph;
-            image = (element.multimedia.length == 0) ? 'image' : `http://www.nytimes.com/${element.multimedia[0].url}`;
-            time = (element.pub_date ==null) ? '2022' : element.pub_date;
-            createUppend(topic,head,abstract,content,`${image}`,time);
-        }
-    }
-    else{
-        let arr=[err,wrong];
-        setheader('Error: try again after some time ',searchNewsarr.length,arr);
-    }
+.sidebar{
+    position: fixed;
+    display: block;
+    bottom: 200px;
+    left: 0px;
+    width: fit-content;
+    height: fit-content;
+    background-color: rgba(0, 0, 0, 0.753);
+    border-bottom-right-radius: 20px;
+    border-top-right-radius: 20px;
+    align-items: center;
+    padding: 10px;
 }
-
-
-// ********************************************************************************
-//scroll up button
-let progressBar = document.getElementsByClassName('progress-bar')[0];
-window.onscroll = ()=>{
-    if(document.documentElement.scrollTop > 200)
-        document.getElementsByClassName('upbtndiv')[0].style.display='block';
-    else
-    document.getElementsByClassName('upbtndiv')[0].style.display='none';
-    
-    
-    //*********************************************************************************
-    // progress bar
-    let getheight = document.documentElement.scrollTop;
-    let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    progressBar.style.width=`${(getheight/totalHeight)*100}%`;
+.sidebarlink{
+    display: none;
 }
-
-function getscrollup(){
-        document.documentElement.scrollTop=0;
-    }
-
-    
-    
-
-//********************************************************************************
-//slidebar for sharing
-function toggleside(){
-    let sidebarlink =   document.getElementsByClassName('sidebarlink')[0];
-    (sidebarlink.style.display=='none') ? sidebarlink.style.display='block' : sidebarlink.style.display='none'
-
+.sidebar h2 , .sidebar a{
+   color: white;
+}
+.sidebar a:hover{
+    color: rgb(128, 128, 172);
 }
